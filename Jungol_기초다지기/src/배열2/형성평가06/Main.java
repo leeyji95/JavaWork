a2d1a4abb1097f1a305711ba9ea8d389a61da8f9
leeyji95 <leeyji95@naver.com>
2020-04-28 12:55:44 +0900

    04.28 12:55

---
+++ Jungol_기초다지기/src/배열2/형성평가06/Main.java
@@ -0,0 +1,20 @@
+package 배열2.형성평가06;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int [] row = new int[5];
+		int [] col = new int[5];
+		
+//		int[0]
+		
+		for (int i = 0; i < row.length; i++) {
+			
+			
+		}
+		
+		
+	} // end main
+
+} // end class
