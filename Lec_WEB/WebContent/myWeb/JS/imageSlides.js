19d112e110a565e3bc410f76aa75c175f6953a52
leeyji95 <leeyji95@naver.com>
2020-05-14 01:00:07 +0900

    2020.05.14 1:10

--- Lec_WEB/WebContent/myWeb/JS/imageSlides.js
+++ Lec_WEB/WebContent/myWeb/JS/imageSlides.js
@@ -1,71 +1,71 @@
-var slideIndex = 0;
-showSlides(slideIndex);
+// var slideIndex = 0;
+// showSlides(slideIndex);
 
-function plusSlides(n){
-  slideIndex += n; /* 현재 인덱스 번호에서 추가 */
-  showSlides(slideIndex);
-}
-function currentSlide(n) { // 현재 슬라이드 인덱스 번호 
-  slideIndex = n;
-  showSlides(slideIndex);
-}
+// function plusSlides(n){
+//   slideIndex += n; /* 현재 인덱스 번호에서 추가 */
+//   showSlides(slideIndex);
+// }
+// function currentSlide(n) { // 현재 슬라이드 인덱스 번호 
+//   slideIndex = n;
+//   showSlides(slideIndex);
+// }
 
 
-function showSlides(n) {
-  var i;
-  slideIndex = n;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
+// function showSlides(n) {
+//   var i;
+//   slideIndex = n;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
 
-  slideIndex++;
+//   slideIndex++;
 
-  if (slideIndex > slides.length) {slideIndex = 1}    /* 처음부터 다시 시작 */
-  if(slideIndex < 0) {slideIndex = 1}
+//   if (slideIndex > slides.length) {slideIndex = 1}    /* 처음부터 다시 시작 */
+//   if(slideIndex < 0) {slideIndex = 1}
 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
 
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 1000); // Change image every 2 seconds
-}
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 1000); // Change image every 2 seconds
+// }
 
-//--------------------------------------------------------------------------------------
+// //--------------------------------------------------------------------------------------
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
 
-  setTimeout(showSlides, 1000); // Change image every 2 seconds
-}
+//   setTimeout(showSlides, 1000); // Change image every 2 seconds
+// }
 
 
 //----------------------------------------------------------------------
@@ -86,5 +86,6 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
