7221fd8855e01b8a98f3016dea915b483621cccc
leeyji95 <leeyji95@naver.com>
2020-07-07 13:15:03 +0900

    2020.0707

---
+++ STS18_Security/src/test/java/com/lec/sts18_security/TestJunit1.java
@@ -0,0 +1,14 @@
+package com.lec.sts18_security;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestJunit1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
